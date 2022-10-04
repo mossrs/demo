3ffecbe0325a5f1429938c45f81755ecc2f47492
zsb <1326739216@qq.com>
2022-10-04 14:10:51 +0800

    增加dto

---
+++ src/main/java/com/example/demo/dto/MyDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+/**
+ * @author z's'b
+ * @version 1.0
+ * @date 2022/10/4 14:10
+ */
+public class MyDto {
+    private String name;
+    private Integer id;
+    private String address;
+}
