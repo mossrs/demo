cf115c228224337cb6f93a748587b3e5ddf310d4
zsb <1326739216@qq.com>
2022-10-04 14:03:35 +0800

    增加mapper

---
+++ src/main/java/com/example/demo/mapper/MyMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapper;
+
+/**
+ * @author z's'b
+ * @version 1.0
+ * @date 2022/10/4 14:02
+ */
+public class MyMapper {
+
+    public static void main(String[] args) {
+        System.out.println("mapper");
+    }
+}
